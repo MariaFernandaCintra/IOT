29e787273609d3f0a309741d5285432daddaf70d
MariaFernandaCintra <mariafernandaccintra@gmail.com>
2025-09-01 11:27:20 -0300

    Monitoramento de led

--- sensorTempertura/sensorTempertura.ino
+++ sensorTempertura/sensorTempertura.ino
@@ -1,40 +1,42 @@
 #include <math.h>
 #include "AdafruitIO_WiFi.h"
 
-//COnfigurações da rede wifi
-//#define WIFI_SSID ""
-//#define WIFI_PASS ""
+//Configuração da rede WIFI
+#define WIFI_SSID "" 
+#define WIFI_PASS ""
 
-//Autenticação Adafruit IO 
-//#define IO_USERNAME  ""
-//#define IO_KEY       ""
+// Autenticação Adafruit IO
+#define IO_USERNAME ""
+#define IO_KEY ""
 
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-#define pinNTC 34 //Pino ADC onde o NTC está conectado 
-#define pinLed 14 //Pino do led 
+#define pinNTC 34 // pino do sensor de temperatura
+#define pinLed 14 // pino do led
 
-//Controle de envio de dados 
+// Controle de envio de dados 
 float temp_atual = 0;
 float temp_anterior = -1;
 
-//Variavel / ponteiro para referenciar o feed temperatura 
-AdafruitIO_Feed * temperatura = io.feed("Temperatura");
+//Variavel / ponteiro para referenciar o feed temperatura
+AdafruitIO_Feed *temperatura = io.feed("Temperatura");
+AdafruitIO_Feed *ledFeed = io.feed("botaoled");
 
-const float Rfixo = 10000.0; //Resistor do projeto 
-const float Beta = 3950.0;
-const float R0 = 10000.0; //nominal do NTC 
-const float T0_kelvin = 298.15; //25°C em kelvin 
+
+const float Rfixo = 10000.0; // Resistor do projeto 
+const float Beta = 3950.0; 
+const float R0 = 10000.0; // nominal do NTC
+const float T0_kelvin = 298.15; // 25°C em Kelvin
 const float Vcc = 3.3;
 
-void setup() { 
+void setup() {
   pinMode(pinNTC, INPUT);
   pinMode(pinLed, OUTPUT);
   Serial.begin(115200);
 
   while(!Serial);
 
-  Serial.print("Conectando ao Adafruit IO");
+  Serial.print("Conectando ao adafruit IO");
   io.connect();
 
   while(io.status() < AIO_CONNECTED){
@@ -43,25 +45,23 @@ void setup() {
   }
 
   Serial.println();
-  Serial.print(io.statusText());
+  Serial.println(io.statusText());
 
-  //Configuração do callback, quando o feed receber(atualizar) um valor
-  temperatura -> onMessage(handleTemperatura);
-  //Registra a função de callback 
-  //Ela será chamada sempre que o feed receber um novo dado  
-
+  // Configuração do callback, quando o feed receber(atualizar) um valor
+  // temperatura -> onMessage(handleTemperatura);
+  ledFeed->onMessage(handleBotaoLed);
+  //registra a função de callback 
+  // ela será chamada sempre que o feed receber um novo dado
 
   delay(1000);
 }
 
 void loop() {
-
-  //Manter a conexão com Adafruit IO ativa 
+  //Manter a coneção com o Adafruit IO ativa
   io.run();
 
-  //chamada da função publish
-  //publicacao();
+  //publicacao(); //chamada da função publish
 
-  delay(3000);
 
+  delay(1000);
 }
